222d6eac0e3de26472d9fe8364679f796995c95c
김동연 <hsg99091@gmail.com>
2021-03-26 09:58:56 +0900

    DAO 중간 커밋

---
+++ ServletEx/src/com/bit/servlet/dao/EmailDao.java
@@ -0,0 +1,12 @@
+package com.bit.servlet.dao;
+
+import java.util.List;
+
+//	추상 메소드의 선언
+public interface EmailDao {
+	
+	public List<EmailVo> getList();		//	SELECT
+	public int insert(EmailVo vo);		//	INSERT
+	public int delete(Long no);			//	DELETE by PK
+	
+}
